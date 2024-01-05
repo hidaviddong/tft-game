092afcb5b00d3fd64ba556606ae8214b5b7ca84e
david dong <hi@daviddong.me>
2024-01-05 14:36:17 +0800

    chore: dep update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,7 @@
-import { useAtom } from 'jotai'
-import { Link } from 'react-router-dom'
-
-import { Button } from '@/components/ui/button'
-import { counterAtom } from '@/store'
 export default function Home() {
-  const [count, setCount] = useAtom(counterAtom)
   return (
-    <div className="h-40 w-40">
-      <h1 className="text-bold text-3xl">Jotai</h1>
-      <Button onClick={() => setCount(count + 1)}>Click me {count}</Button>
-      <Link to="/about">About</Link>
+    <div className="flex h-full w-full items-center justify-center bg-sky-50">
+      <h1>Hello</h1>
     </div>
   )
 }
