d4038ccde9df3a12e894fbb40de459b2e130c875
david dong <hi@daviddong.me>
2024-01-10 09:16:03 +0800

    faet: add loader icon

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,10 @@
+import { Loader } from 'lucide-react'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { useCountdown, useTodayHero } from '@/hooks'
 
 import BenchCards from './components/benchcards'
 import HandCards from './components/handcards'
-
 export default function Home() {
   const { data, isLoading, isError, isSuccess } = useTodayHero()
   const { time, startCountdown, resetCountdown } = useCountdown(3)
@@ -12,8 +14,13 @@ export default function Home() {
       <button onClick={startCountdown}>开始挑战</button>
       <button onClick={resetCountdown}>再次挑战</button>
       {time === 0 ? '时间到!' : <span>{time}</span>}
-      {isLoading && <h1>加载今日英雄中</h1>}
-      {isSuccess && <h1>{data}</h1>}
+      {isLoading && <Loader className="animate-spin" />}
+      {isSuccess && (
+        <Avatar className="h-16 w-16">
+          <AvatarImage className="h-16 w-16" src={`https://imgtft.daviddong.me/${data}.png`} />
+          <AvatarFallback> {data}</AvatarFallback>
+        </Avatar>
+      )}
       {isError && <h1>加载今日英雄错误!</h1>}
       <BenchCards />
       <HandCards />
